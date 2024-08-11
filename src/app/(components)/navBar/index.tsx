6282ad07f9a25928062a029443363d6b22127207
shivamgoyal26 <officialshivamgoyal@gmail.com>
2024-08-11 10:45:10 +0530

    Sidebar header added

--- src/app/(components)/navBar/index.tsx
+++ src/app/(components)/navBar/index.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full mb-7">
       {/* LEFT SIDE */}
-      <div className="flex justify-between items-center gap-5">
+      <div className="flex  items-center gap-5">
         <button
           className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
           onClick={() => {
--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "./(components)/navBar";
+import SideBar from "./(components)/sideBar";
 
 type DashboardWrapperProps = {
   children: React.ReactNode;
@@ -8,7 +9,7 @@ type DashboardWrapperProps = {
 const DashboardWrapper = ({ children }: DashboardWrapperProps) => {
   return (
     <div className="light flex bg-gray-50 text-gray-900 w-full min-h-screen">
-      SideBar
+      <SideBar />
       <main className="flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24">
         <NavBar />
         {children}
